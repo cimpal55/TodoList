97219c75784cdbd3ffd900baef1fd2c77bf15319
vladislavs.suspanovs <vladislavs.suspanovs@DAYTON049>
2022-07-08 18:16:35 +0300

    Add project files.

---
+++ TodoList/MVVM/Models/ToDoModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TodoList.Core;
+
+namespace TodoList.MVVM.Models
+{
+    internal class ToDoModel : ObservableObject
+    {
+        private string _description;
+
+        private bool _isCompleted;
+
+        [JsonProperty(PropertyName = "creationDate ")]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [JsonProperty(PropertyName = "description ")]
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (_description == value)
+                    return;
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        [JsonProperty(PropertyName = "isCompleted ")]
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                if (_isCompleted == value)
+                    return;
+                _isCompleted = value;
+                OnPropertyChanged("IsCompleted");
+            }
+        }
+    }
+}
+ 
